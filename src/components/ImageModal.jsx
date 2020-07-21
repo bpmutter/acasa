c2ab650c17ae59c9d049f91cd486b339b51250aa
Ben Perlmutter <benperlmutter94@gmail.com>
2020-07-21 15:50:13 -0500

    add image expansion for img gallery

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -2,30 +2,31 @@ import React, {useState} from 'react';
 import {Modal, makeStyles} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+
   img: {
-    height: 120,
-    width: 120,
+    height: '100%',
+    width: '100%',
     backgroundPosition: "center",
     backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundColor: "pink",
-    cursor: 'pointer',
-    '&:hover': {
-        position: 'relative',
-        transform: 'scale(1.1)', 
-    }
+    // backgroundRepeat: "no-repeat",
+    // backgroundColor: "pink",
+    cursor: "pointer",
   },
   modalFrame: {
     display: "inline-block",
     outlineColor: theme.palette.secondary.light,
-    backgroundColor: 'white',
+    backgroundColor: "white",
+  },
+  imgModal: {
+    maxHeight: 600,
+    maxWidth: 900,
   },
 }));
 
 
-export default function ImageModal({ src, alt, className }) {
+export default function ImageModal({ src, alt, component, className }) {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
     setOpen(true);
@@ -36,10 +37,29 @@ export default function ImageModal({ src, alt, className }) {
   };
 
   
-
+  if(component === 'img'){
+      return (<>
+        <img src={src} alt={alt}/>
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div className={classes.modalFrame}>
+              <img src={src} alt={alt} className={classes.imgModal}></img>
+            </div>
+        </Modal>
+      </>);
+  }
 
   return (
-    <div>
+    <div className={classes.imgWrapper}>
       <div
         className={className ? `${classes.img} ${className}` : classes.img}
         style={{ backgroundImage: `url(${src})` }}
@@ -54,7 +74,7 @@ export default function ImageModal({ src, alt, className }) {
         style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}
       >
         <div className={classes.modalFrame}>
-            <img src={src} alt='homer'></img>
+            <img src={src} alt={alt} className={classes.imgModal}></img>
         </div>
       </Modal>
     </div>
--- src/components/ListingPhotos.jsx
+++ src/components/ListingPhotos.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Modal} from '@material-ui/core';
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import ContentPaper from "./ContentPaper";
-
+import ImageModal from './ImageModal';
 
 const useStyles = makeStyles((theme) => ({
   gridListRoot: {
@@ -18,39 +19,81 @@ const useStyles = makeStyles((theme) => ({
     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
     transform: "translateZ(0)",
   },
+  gridImg: {
+      cursor: 'pointer',
+  },
+  modalFrame: {
+    display: "inline-block",
+    outlineColor: theme.palette.secondary.light,
+    backgroundColor: "white",
+  },
+  imgModal: {
+    maxHeight: 600,
+    maxWidth: 900,
+  },
+  primaryImgWrapper: {
+    "&:hover": {
+      position: "relative",
+      zIndex: 400,
+      transform: "scale(1.05)",
+    },
+  },
   primaryImg: {
     width: 500,
     height: 300,
     backgroundPosition: "center",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-    [theme.breakpoints.down("md")]: {
-      marginBottom: theme.spacing(5),
-      height: 250,
-      width: "auto",
-    },
-
-    [theme.breakpoints.down("sm")]: {
-      width: 300,
-    },
   },
   spacing: {
-      margin: theme.spacing(1)
-  }
+    margin: theme.spacing(1),
+  },
 }));
 
 function SingleLineGridList({imgs, listingTitle}) {
   const classes = useStyles();
-  console.log('imgs are::',imgs)
+  const [open, setOpen] = useState(false);
+  const [modalSrc,  setModalSrc] = useState('');
+  const [modalAlt, setModalAlt] = useState('');
+
+  const handleOpen = (e) => {
+    setOpen(true);
+    setModalSrc(e.target.src)
+    setModalAlt(e.target.alt)
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div className={classes.gridListRoot}>
       <GridList className={classes.gridList} cols={3.5}>
-        {imgs.map((imgLink, i) => (
-          <GridListTile key={imgLink}>
-            <img src={imgLink} alt={`${listingTitle} img ${i + 1}`} />
+        {imgs.map((src, i) => (
+          <GridListTile key={src}>
+            <img src={src} 
+              alt={`${listingTitle} img ${i + 1}`} 
+              onClick={handleOpen}
+              className={classes.gridImg}
+            />
+             
           </GridListTile>
         ))}
       </GridList>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div className={classes.modalFrame}>
+          <img src={modalSrc} alt={modalAlt} className={classes.imgModal}></img>
+        </div>
+      </Modal>
     </div>
   );
 }
@@ -60,12 +103,18 @@ export default function ListingPhotos({primaryImg, additionalImgs, title, ...pro
     const classes = useStyles();
     return (
       <section {...props}>
-        <ContentPaper style={{marginBottom: '1em', maxWidth:500, margin: '0 auto'}}>
-          <div
+        <Paper className={classes.primaryImgWrapper}
+        style={{marginBottom: '1em', maxWidth:500, margin: '0 auto', padding: '.5em'}}>
+          {/* <div
             className={classes.primaryImg}
             style={{ backgroundImage: `url(${primaryImg})` }}
-          ></div>
-        </ContentPaper>
+          ></div> */}
+          <ImageModal
+              src={primaryImg}
+              alt={title}
+              className={classes.primaryImg}
+          />
+        </Paper>
         <div className={classes.spacing}>
         </div>
         {additionalImgs && additionalImgs.length && (
